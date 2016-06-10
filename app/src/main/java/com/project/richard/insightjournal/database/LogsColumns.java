83e110d489c99b06a3f1130b707639bdc2f4469f
Richard Yee <rdyyc99@gmail.com>
2016-06-10 15:33:33 -0700

    Added content provider database with the schematic library

--- app/src/main/java/com/project/richard/insightjournal/database/LogsColumns.java
+++ app/src/main/java/com/project/richard/insightjournal/database/LogsColumns.java
@@ -1,20 +1,18 @@
-//package com.project.richard.insightjournal.database;
-//
-//import net.simonvt.schematic.annotation.AutoIncrement;
-//import net.simonvt.schematic.annotation.DataType;
-//import net.simonvt.schematic.annotation.NotNull;
-//import net.simonvt.schematic.annotation.PrimaryKey;
-//
-//import static net.simonvt.schematic.annotation.DataType.Type.*;
-//
-///**
-// * Created by a11 on 6/4/16.
-// */
-//public interface LogsColumns {
-//    @DataType(INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
-//    @DataType(TEXT) @NotNull String TITLE = "title";
-//    @DataType(TEXT) @NotNull String DATETIME = "datetime";
-//    @DataType(INTEGER) @NotNull String HOUR = "hour";
-//    @DataType(INTEGER) @NotNull String MINUTE = "minute";
-//    @DataType(TEXT) String JOURNAL_ENTRY = "journal_entry";
-//}
+package com.project.richard.insightjournal.database;
+
+import net.simonvt.schematic.annotation.AutoIncrement;
+import net.simonvt.schematic.annotation.DataType;
+import net.simonvt.schematic.annotation.NotNull;
+import net.simonvt.schematic.annotation.PrimaryKey;
+
+import static net.simonvt.schematic.annotation.DataType.Type.*;
+
+/**
+ * Created by a11 on 6/4/16.
+ */
+public interface LogsColumns {
+    @DataType(INTEGER) @PrimaryKey @AutoIncrement String _ID = "_id";
+    @DataType(TEXT) @NotNull String DATETIME_UTC = "datetime_utc";
+    @DataType(TEXT) @NotNull String TITLE = "title";
+    @DataType(TEXT) String JOURNAL_ENTRY = "journal_entry";
+}
--- app/src/main/java/com/project/richard/insightjournal/database/LogsDatabase.java
+++ app/src/main/java/com/project/richard/insightjournal/database/LogsDatabase.java
@@ -1,28 +1,31 @@
-//package com.project.richard.insightjournal.database;
-//
-//import android.content.Context;
-//import android.database.sqlite.SQLiteDatabase;
-//
-//import net.simonvt.schematic.annotation.OnConfigure;
-//import net.simonvt.schematic.annotation.OnCreate;
-//import net.simonvt.schematic.annotation.OnUpgrade;
-//import net.simonvt.schematic.annotation.Table;
-//
-///**
-// * Created by a11 on 6/4/16.
-// */
-//public final class LogsDatabase {
-//    public static final int VERSION = 1;
-//
-//    @Table(LogsColumns.class) public static final String LOGS = "logs";
-//
-//    @OnCreate public static void onCreate(Context context, SQLiteDatabase db) {
-//    }
-//
-//    @OnUpgrade public static void onUpgrade(Context context, SQLiteDatabase db, int oldVersion,
-//                                            int newVersion) {
-//    }
-//
-//    @OnConfigure public static void onConfigure(SQLiteDatabase db) {
-//    }
-//}
+package com.project.richard.insightjournal.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import net.simonvt.schematic.annotation.Database;
+import net.simonvt.schematic.annotation.IfNotExists;
+import net.simonvt.schematic.annotation.OnConfigure;
+import net.simonvt.schematic.annotation.OnCreate;
+import net.simonvt.schematic.annotation.OnUpgrade;
+import net.simonvt.schematic.annotation.Table;
+
+/**
+ * Created by a11 on 6/4/16.
+ */
+@Database(version = LogsDatabase.VERSION)
+public final class LogsDatabase {
+    public static final int VERSION = 1;
+
+    @Table(LogsColumns.class)@IfNotExists public static final String LOGS = "logs";
+
+    @OnCreate public static void onCreate(Context context, SQLiteDatabase db) {
+    }
+
+    @OnUpgrade public static void onUpgrade(Context context, SQLiteDatabase db, int oldVersion,
+                                            int newVersion) {
+    }
+
+    @OnConfigure public static void onConfigure(SQLiteDatabase db) {
+    }
+}
