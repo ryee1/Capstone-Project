1bfe4386335849e78e538865e59de428c9a6d08d
Richard Yee <rdyyc99@gmail.com>
2016-08-19 18:48:22 -0700

    Added skeleton code for admob

--- app/src/main/java/com/project/richard/insightjournal/ui/mainpagerscreen/linkscreen/LinksFragment.java
+++ app/src/main/java/com/project/richard/insightjournal/ui/mainpagerscreen/linkscreen/LinksFragment.java
@@ -3,11 +3,18 @@ package com.project.richard.insightjournal.ui.mainpagerscreen.linkscreen;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.MobileAds;
 import com.project.richard.insightjournal.R;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -26,11 +33,19 @@ public class LinksFragment extends Fragment {
     }
 
 
+    @BindView(R.id.adView) AdView mAdView;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_resources, container, false);
+        View view =  inflater.inflate(R.layout.fragment_resources, container, false);
+        ButterKnife.bind(this, view);
+        Log.e("adcheck", getString(R.string.banner_ad_unit_id));
+        MobileAds.initialize(this, getString(R.string.banner_ad_unit_id));
+        AdRequest adRequest = new AdRequest.Builder().build();
+        mAdView.loadAd(adRequest);
+        return view;
     }
 
 }
